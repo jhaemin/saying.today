9f23bef3dd6c84d5071d0ca3df86b96158700d41
jhaemin <io@jhaemin.com>
2020-10-19 22:27:30 +0900

    chore: fix typo and replace og image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,9 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>1일1명언 - 쎄잉 (Saying)</title>
+        <title>1일 1명언 - 쎄잉 (Saying)</title>
         <meta name="description" content="1일 1명언으로 의미 있는 하루를" />
-        <meta property="og:title" content="1일1명언 - 쎄잉 (Saying)" />
+        <meta property="og:title" content="1일 1명언 - 쎄잉 (Saying)" />
         <meta
           property="og:description"
           content="1일 1명언으로 의미 있는 하루를"
