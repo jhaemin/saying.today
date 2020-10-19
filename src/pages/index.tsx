29ee78707616b6261a50d3ada9b2d390d6350beb
jhaemin <io@jhaemin.com>
2020-10-19 11:03:47 +0900

    refactor: typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return <div className="home"></div>
+}
+
+export default Home
