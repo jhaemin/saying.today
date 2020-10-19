65584706e3b829f2abff708cc3d3fc73f2a282e2
jhaemin <io@jhaemin.com>
2020-10-19 22:57:35 +0900

    feat: integrate google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,26 @@ function App({ Component, pageProps }: AppProps) {
           content="1일 1명언으로 의미 있는 하루를"
         />
         <meta property="og:image" content="https://saying.today/og-image.png" />
+        {/* Google Analytics */}
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=UA-140443623-2"
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-140443623-2');
+  `,
+              }}
+            ></script>
+          </>
+        )}
       </Head>
       <Component {...pageProps} />
     </>
