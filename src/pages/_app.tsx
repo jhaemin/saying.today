8212204fa190d8a1a6465440b7cccec759672ea7
jhaemin <io@jhaemin.com>
2020-10-20 21:07:12 +0900

    feat: favicon
    * Support Apple devices
    * Support microsoft tile
    * Support dedicated icon for Android

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,27 @@ function App({ Component, pageProps }: AppProps) {
           content="1일 1명언으로 의미 있는 하루를"
         />
         <meta property="og:image" content="https://saying.today/og-image.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
         {/* Google Analytics */}
         {process.env.NODE_ENV === 'production' && (
           <>
