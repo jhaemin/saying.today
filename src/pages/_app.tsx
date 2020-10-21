8522b16e99707255c49020ed87d97b8c92524849
jhaemin <io@jhaemin.com>
2020-10-21 18:15:01 +0900

    fix: set safari-pinned-tab color to pure black

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,7 +34,7 @@ function App({ Component, pageProps }: AppProps) {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
         {/* Google Analytics */}
