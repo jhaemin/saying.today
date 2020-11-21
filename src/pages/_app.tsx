881001a039e986fbb90df6f67c340f560c724a7c
jhaemin <io@jhaemin.com>
2020-11-21 23:47:40 +0900

    fix: change google analytics script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,18 +41,16 @@ function App({ Component, pageProps }: AppProps) {
         {process.env.NODE_ENV === 'production' && (
           <>
             <script
-              async
-              src="https://www.googletagmanager.com/gtag/js?id=UA-140443623-2"
-            ></script>
-            <script
               dangerouslySetInnerHTML={{
                 __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-gtag('config', 'UA-140443623-2');
-  `,
+ga('create', 'UA-140443623-2', 'auto');
+ga('send', 'pageview');
+              `,
               }}
             ></script>
           </>
